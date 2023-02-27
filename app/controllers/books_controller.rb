f1d21597bcf024f450b1c89872c9f9f67e2bdb3e
lisanyny <110669469+lisanyny@users.noreply.github.com>
2023-02-27 05:09:53 +0000

    フラッシュメッセージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      redirect_to books_path
+      render :index
     end
   end
 
@@ -29,9 +30,10 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
       redirect_to book_path(@book.id)
     else
-      redirect_to edit_book_path(@book.id)
+      render :edit
     end
   end
 
